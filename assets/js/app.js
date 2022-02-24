0ba7acf18f175ab3075cd3f1ff94e58ac3dc7ca3
DJoker <datjoker96@gmail.com>
2022-02-24 17:04:36 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,38 @@
-import { listSong } from "../../data/songPlaylist";
+$(document).ready(function(){
+    $('.dj-sliders').slick({
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        infinite: true,
+        arrows: true,
+        draggable: true,
+        prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="bi bi-chevron-left"></i></button>`,
+        nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="bi bi-chevron-right"></i></button>`,
+        dots: false,
+        
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
 
-console.log(listSong);
+    });
+  });
